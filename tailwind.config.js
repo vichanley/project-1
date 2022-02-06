2d2e045aa2188228bc53e1529b1253b9d42e729f
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-02-06 17:01:07 -0600

    fontawesome link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 tailwind.config = {
   content: ['./index.html'],
   theme: {
-    extend: {
-      colors: {
-        'primary': '#142429',
-        'secondary': '#1B3137',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
