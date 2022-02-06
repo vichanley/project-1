4cdf5c68bf8428c47a517828bee56a4a0777e2fb
grayweling <jy6228@yahoo.com>
2022-02-06 09:06:33 -0600

    removed erroneous code

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -1,24 +1,3 @@
-// var settings = {
-//     "url": "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=7ac48de2&app_key=38100359b40740841a18a00837f9be68",
-//     "method": "GET",
-//     "timeout": 0,
-//   };
-  
-//   $.ajax(settings).done(function (response) {
-//     console.log(response);
-//   });
-//var ingredientSearch = document.getElementById("ingredients").value;
-
-// var requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow'
-//   };
-  
-//   fetch("https://api.edamam.com/api/recipes/v2?type=public&q=biryani&app_key=38100359b40740841a18a00837f9be68&app_id=7ac48de2", requestOptions)
-//     if (response.ok) {
-//         response.json().then
-//     }
-
 var userFormEl = document.querySelector("#user-form")
 var ingredientSearchEl = document.querySelector("#ingredients");
 var appKey = "&app_key=38100359b40740841a18a00837f9be68"
@@ -31,15 +10,6 @@ var getRecipes = function(ingredients) {
 
     //sample url
     //var apiUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=7ac48de2&app_key=38100359b40740841a18a00837f9be68"
-
-    // fetch(apiUrl, {mode:"no-cors"}).then(function(response) {
-    //     response.json().then(function(data) {
-    //         console.log(data)
-    //     })
-    //     .catch(function(error) {
-    //         alert("unable to connect to edamam")
-    //     })
-    // })
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
